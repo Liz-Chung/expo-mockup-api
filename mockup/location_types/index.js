669cab9f295746a0bceff2167856c17a870d7268
Liz Chung <lizzhongx@gmail.com>
2024-07-05 18:17:12 +0900

    no message

--- mockup/location_types/index.js
+++ mockup/location_types/index.js
@@ -4,6 +4,10 @@ import cors, { runMiddleware } from '../../corsMiddleware';
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
 
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+
   if (req.method === 'GET') {
     res.status(200).json(location_types);
   } else {
--- mockup/users/index.js
+++ mockup/users/index.js
@@ -4,5 +4,9 @@ import cors, { runMiddleware } from '../../corsMiddleware';
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
 
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+
   res.status(200).json(users);
 }
